2f76bc55f324c2f309b19a8e7c1a375f307b2923
EfratCohen100 <45036748+EfratCohen100@users.noreply.github.com>
2019-05-30 16:02:24 +0300

    Update signals.c

--- signals.c
+++ signals.c
@@ -1,1 +1,22 @@
 
+// C program to illustrate 
+// pipe system call in C 
+// shared by Parent and Child 
+#include <stdio.h> 
+#include <stdlib.h>
+#include <unistd.h> 
+#include <sys/types.h>
+#include <sys/wait.h>
+#include "md5.h"
+#include <iostream>
+#include <mutex>
+#define  MSGSIZE 20 
+using namespace std;
+
+int main(){
+  
+  printf("test");
+  
+  exit(0);
+  
+}
